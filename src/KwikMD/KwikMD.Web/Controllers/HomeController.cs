f616714a5660ff22f39450f3aa03230e519de955
Jordan Knight <jakkaj@gmail.com>
2016-09-16 19:52:41 +1000

    Added quick md sample

---
+++ src/KwikMD/KwikMD.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace KwikMD.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Convert(string mdString)
+        {
+            //var md = new Markdown().Transform(mdString);
+            var md = CommonMark.CommonMarkConverter.Convert(mdString);
+            ViewData["converted"] = md;
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
